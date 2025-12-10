9c61a1596b0a3f732d83757daa26a2461d982509
pa1808 <paula.l.alcala@gmail.com>
2025-12-10 17:19:57 +0100

    controller guardar completado

--- src/main/java/com/example/controllers/EstudianteController.java
+++ src/main/java/com/example/controllers/EstudianteController.java
@@ -1,14 +1,17 @@
 package com.example.controllers;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entities.Correo;
 import com.example.entities.Estudiante;
 import com.example.entities.Facultad;
+import com.example.entities.Telefono;
 import com.example.services.CorreoService;
 import com.example.services.EstudianteService;
 import com.example.services.FacultadService;
@@ -18,6 +21,7 @@ import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -41,7 +45,7 @@ public class EstudianteController {
 
         return "listadoEstudiantes";
         
-    };
+    }
 
     @GetMapping("/alta")
     public String altaEstudiante(Model model) {
@@ -55,5 +59,45 @@ public class EstudianteController {
         return "formularioAltaModificacion";
     }
     
+    @PostMapping("/guardar")
+    public String guardarEstudiante(@ModelAttribute Estudiante estudiante, 
+        @RequestParam(name = "numerosTelefono", required = false) String telefonos,
+        @RequestParam(name = "direccionesCorreo", required = false) String correos) {
 
+        estudianteService.savEstudiante(estudiante);
+
+        if(telefonos!=null){
+
+            String[] arrayTelefonos = telefonos.split(";");
+            List<String> listadoTelefonos = Arrays.asList(arrayTelefonos);
+
+            listadoTelefonos.forEach(t -> {
+
+                telefonoService.saveTelefono(
+                    Telefono.builder()
+                    .numero(t.trim())
+                    .estudiante(estudiante)
+                    .build()
+                );
+            });
+        }
+
+        if(correos!=null){
+
+            String[] arrayCorreos = correos.split(";");
+            List<String> listadoCorreos = Arrays.asList(arrayCorreos);
+
+            listadoCorreos.forEach(c -> {
+                correoService.saveCorreo(
+                    Correo.builder()
+                    .direccion(c.trim())
+                    .estudiante(estudiante)
+                    .build()
+                );
+            });
+        }
+
+        return "redirect:/estudiantes/listar";
+    }
+    
 }
